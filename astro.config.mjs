a2413a0c3fab249eb93e0513194ec1dd60ae61cb
brohudev <hitarth.thanki9006@gmail.com>
2024-09-19 18:40:48 -0500

    do some things?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), markdoc()]
 });
