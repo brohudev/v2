c873eb0fc199a869406e1eacee8138c6c43fc366
Brohudev <hitarth.thanki9006@gmail.com>
2025-01-07 21:39:34 -0600

    add rosepine colors to tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				Base: '#191724',
+				Overlay: '#26233A',
+				Text: '#E0DEF4',
+				Iris: '#C4A7E7',
+				Surgace: '#1F1D2E',
+				Muted: '#6E6A86',
+				Subtle: '#6E6A86',
+				Love: '#EB6F92',
+				Gold: '#F6C177',
+				Rose: '#EBBCBA',
+				Pine: '#31748F',
+				Foam: '#9CCFD8',
+				HighlightLow: '#9CCFD8',
+				HighlightMed: '#403D52',
+				HighlightHigh: '#524F67',
+
+			},
+		},
 	},
 	plugins: [],
 }
