146df4632a1a6ee6e1f86ca6a860bb89149f023d
Brohudev <hitarth.thanki9006@gmail.com>
2025-01-24 13:04:59 -0600

    formalize the schema for writing.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,12 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const writingCollection = defineCollection({
+	loader: glob({
+		pattern: "**/*.md",
+		base: "./src/content/writing",
+		generateId: ({ entry }) => entry.replace(/\.md$/, '')
+	}),
 	schema: z.object({
 		title: z.string(),
 		date: z.string(),
