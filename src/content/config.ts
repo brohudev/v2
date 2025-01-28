c510e865d535d364686971b2bc164ed3e9250489
Brohudev <hitarth.thanki9006@gmail.com>
2025-01-27 23:26:18 -0600

    add all the rest of the content collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -17,5 +17,43 @@ const writing = defineCollection({
 	})
 });
 
+
+const projects = defineCollection({
+	loader: glob({
+		pattern: "*.md",
+		base: "./src/content/projects",
+		generateId: ({ entry }) => entry.replace(/\.md$/, '')
+	}),
+	schema: z.object({
+		title: z.string(),
+		github: z.string().url(),
+		demo: z.string().url(),
+		image: z.string(),
+		technologies: z.array(z.string()),
+		tagline: z.string(),
+	})
+});
+
+
+
+const career = defineCollection({
+	loader: glob({
+		pattern: "*.md",
+		base: "./src/content/career",
+		generateId: ({ entry }) => entry.replace(/\.md$/, '')
+	}),
+	schema: z.object({
+		position: z.string(),
+		company: z.string(),
+		fromDate: z.string(),
+		toDate: z.string(),
+		careerType: z.string(),
+		icon: z.string(),
+		location: z.string(),
+		groupIcon: z.string(),
+	})
+});
+
+
 export const collections = { writing };
 
